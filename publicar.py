ca3e3d0a82ff81ac0970b67fb0bc8e813d747e85
unknown <felixtorresjavierantonio@gmail.com>
2026-09-16 16:14:05 -0700

    update

--- publicar.py
+++ publicar.py
@@ -3,7 +3,7 @@ import json
 import requests
 
 PAGE_ID = os.environ.get("PAGE_ID")
-ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN_2")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN_3")
 
 JSON_FILE = "publicaciones.json"
 
